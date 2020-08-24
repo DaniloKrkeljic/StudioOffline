5ffb60b2b4d9e0f344bbfdb0cfaec79240dbbdea
= <=>
2020-08-24 18:07:14 +0200

    center

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let h3 = document.getElementById('naslov');
 
 rect = bjelilo.getBoundingClientRect();
 
-bjelilo.addEventListener('mousemove touchmove',(e) => {
+bjelilo.addEventListener('mousemove',(e) => {
   cursorNodeH.style.top = e.clientY + 'px';
   cursorNodeV.style.left = e.pageX + 'px';
 
