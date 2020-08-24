cfa2af8225d2780a9d72692a72129b683fb58bd0
= <=>
2020-08-24 17:52:54 +0200

    Verzija 0.90001

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let cursorNodeH = document.getElementById('crosshair-h')
+let cursorNodeV = document.getElementById('crosshair-v');
+
+
+let bjelilo = document.getElementById('bjelilo');
+let h3 = document.getElementById('naslov');
+
+rect = bjelilo.getBoundingClientRect();
+
+bjelilo.addEventListener('mousemove',(e) => {
+  cursorNodeH.style.top = e.clientY + 'px';
+  cursorNodeV.style.left = e.pageX + 'px';
+
+  // console.log(e.pageX, e.offsetY)
+});
+
+
+
+
