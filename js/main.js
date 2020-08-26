3c536540812cba44e1f12e9abfb7f52f8d67fc3d
= <=>
2020-08-26 15:41:57 +0200

    mobile touchmove added

--- js/main.js
+++ js/main.js
@@ -7,20 +7,20 @@ let h3 = document.getElementById('naslov');
 
 rect = bjelilo.getBoundingClientRect();
 
-bjelilo.addEventListener("mousemove",(e) => {
-  cursorNodeH.style.top = e.clientY + 'px';
-  cursorNodeV.style.left = e.pageX + 'px';
-
-  // console.log(e.pageX, e.offsetY)
-});
-
-bjelilo.addEventListener("touchmove",(e) => {
-  cursorNodeH.style.top = e.clientY + 'px';
-  cursorNodeV.style.left = e.pageX + 'px';
-
-  // console.log(e.pageX, e.offsetY)
-});
-
-
+if(window.innerWidth <= 480){
+  bjelilo.addEventListener("touchmove",(e) => {
+    cursorNodeH.style.top = e.clientY + 'px';
+    cursorNodeV.style.left = e.pageX + 'px';
+  
+    // console.log(e.pageX, e.offsetY)
+  });
+} else {
+  bjelilo.addEventListener("mousemove",(e) => {
+    cursorNodeH.style.top = e.clientY + 'px';
+    cursorNodeV.style.left = e.pageX + 'px';
+  
+    // console.log(e.pageX, e.offsetY)
+  });
+}
 
 
